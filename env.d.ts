d388463b56e84c3679372352b6d90040a0111e87
Kerwin Jhong <kerwinjhong2025@gmail.com>
2025-10-15 13:42:08 +0800

    Random sessionId

--- env.d.ts
+++ env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
     readonly VITE_AWS_REGION: string
     readonly VITE_AWS_BEDROCK_AGENT_ARN: string
-    readonly VITE_AWS_BEDROCK_SESSION_ID: string
 }
 
 interface ImportMeta {
